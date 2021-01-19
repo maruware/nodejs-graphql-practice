89dcc0e1f2e9ab41fb2d7ac2285485ba66907dc5
maruware <me@maruware.com>
2021-01-19 16:48:57 +0900

    Constructing Types

--- src/index.ts
+++ src/index.ts
@@ -1,142 +1,54 @@
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
-const { buildSchema } = require('graphql')
+const graphql = require('graphql')
 
-// Construct a schema, using GraphQL schema language
-const schema = buildSchema(`
-  type RandomDie {
-    numSides: Int!
-    rollOnce: Int!
-    roll(numRolls: Int!): [Int]
-  }
-
-  input MessageInput {
-    content: String
-    author: String
-  }
- 
-  type Message {
-    id: ID!
-    content: String
-    author: String
-  }
-
-  type Query {
-    quoteOfTheDay: String
-    random: Float!
-    rollThreeDice: [Int]
-    rollDice(numDice: Int!, numSides: Int): [Int]
-    getDie(numSides: Int): RandomDie
-    getMessage(id: ID!): Message
-    ip: String
-  }
-
-  type Mutation {
-    createMessage(input: MessageInput): Message
-    updateMessage(id: ID!, input: MessageInput): Message
-  }
-`)
-
-class RandomDie {
-  numSides: number
-  constructor(numSides: number) {
-    this.numSides = numSides
-  }
-
-  rollOnce() {
-    return 1 + Math.floor(Math.random() * this.numSides)
-  }
-
-  roll({ numRolls }: { numRolls: number }) {
-    const output = []
-    for (let i = 0; i < numRolls; i++) {
-      output.push(this.rollOnce())
-    }
-    return output
-  }
-}
-
-class Message {
-  id: string
-  content: string
-  author: string
-  constructor(
-    id: string,
-    { content, author }: { content: string; author: string }
-  ) {
-    this.id = id
-    this.content = content
-    this.author = author
-  }
-}
-
-const fakeDatabase: any = {}
-
-// The root provides a resolver function for each API endpoint
-const root = {
-  quoteOfTheDay: () => {
-    return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within'
+// Maps id to User object
+const fakeDatabase = {
+  a: {
+    id: 'a',
+    name: 'alice',
   },
-  random: () => {
-    return Math.random()
-  },
-  rollThreeDice: () => {
-    return [1, 2, 3].map(() => 1 + Math.floor(Math.random() * 6))
-  },
-  rollDice: ({ numDice, numSides }: { numDice: number; numSides?: number }) => {
-    const output = []
-    for (let i = 0; i < numDice; i++) {
-      output.push(1 + Math.floor(Math.random() * (numSides || 6)))
-    }
-    return output
-  },
-  getDie: ({ numSides }: { numSides?: number }) => {
-    return new RandomDie(numSides || 6)
-  },
-  getMessage: ({ id }: { id: string }) => {
-    if (!fakeDatabase[id]) {
-      throw new Error('no message exists with id ' + id)
-    }
-    return new Message(id, fakeDatabase[id])
-  },
-  createMessage: ({
-    input,
-  }: {
-    input: { content: string; author: string }
-  }) => {
-    // Create a random id for our "database".
-    const id = require('crypto').randomBytes(10).toString('hex')
-
-    fakeDatabase[id] = input
-    return new Message(id, input)
-  },
-  updateMessage: ({
-    id,
-    input,
-  }: {
-    id: string
-    input: { content: string; author: string }
-  }) => {
-    if (!fakeDatabase[id]) {
-      throw new Error('no message exists with id ' + id)
-    }
-    // This replaces all old data, but some apps might want partial update.
-    fakeDatabase[id] = input
-    return new Message(id, input)
-  },
-  ip: function (_: any, request: any) {
-    return request.ip
+  b: {
+    id: 'b',
+    name: 'bob',
   },
 }
+
+// Define the User type
+const userType = new graphql.GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: { type: graphql.GraphQLString },
+    name: { type: graphql.GraphQLString },
+  },
+})
+
+// Define the Query type
+const queryType = new graphql.GraphQLObjectType({
+  name: 'Query',
+  fields: {
+    user: {
+      type: userType,
+      // `args` describes the arguments that the `user` query accepts
+      args: {
+        id: { type: graphql.GraphQLString },
+      },
+      resolve: (_: any, { id }: { id: string }) => {
+        return fakeDatabase[id]
+      },
+    },
+  },
+})
+
+const schema = new graphql.GraphQLSchema({ query: queryType })
 
 const app = express()
 app.use(
   '/graphql',
   graphqlHTTP({
     schema: schema,
-    rootValue: root,
     graphiql: true,
   })
 )
 app.listen(4000)
-console.log('Running a GraphQL API server at http://localhost:4000/graphql')
+console.log('Running a GraphQL API server at localhost:4000/graphql')
