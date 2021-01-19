b6d1940c7742755f3d4a0192192a2e8ce22cfeb3
maruware <me@maruware.com>
2021-01-19 14:46:26 +0900

    Basic Types

--- src/index.ts
+++ src/index.ts
@@ -5,22 +5,22 @@ const { buildSchema } = require('graphql')
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
   type Query {
-    hello: String
-    rollDice(numDice: Int!, numSides: Int): [Int]
+    quoteOfTheDay: String
+    random: Float!
+    rollThreeDice: [Int]
   }
 `)
 
 // The root provides a resolver function for each API endpoint
 const root = {
-  hello: () => {
-    return 'Hello world!'
+  quoteOfTheDay: () => {
+    return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within'
   },
-  rollDice: ({ numDice, numSides }: { numDice: number; numSides?: number }) => {
-    const output = []
-    for (let i = 0; i < numDice; i++) {
-      output.push(1 + Math.floor(Math.random() * (numSides || 6)))
-    }
-    return output
+  random: () => {
+    return Math.random()
+  },
+  rollThreeDice: () => {
+    return [1, 2, 3].map(() => 1 + Math.floor(Math.random() * 6))
   },
 }
 
